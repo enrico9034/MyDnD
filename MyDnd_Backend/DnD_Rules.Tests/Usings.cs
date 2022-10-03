5782fcd3652bf091f1cce03c54ad8cb67e77336c
davide.gozzi <daddidaffi@gmail.com>
2022-10-03 16:02:15 -0400

    Now the Pladin (example) has the level 2 unlockable

--- MyDnd_Backend/DnD_Rules.Tests/Usings.cs
+++ MyDnd_Backend/DnD_Rules.Tests/Usings.cs
@@ -2,3 +2,12 @@ global using NUnit.Framework;
 global using DnD;
 global using DnD.LuaObjects;
 global using FluentAssertions;
+using FluentAssertions.Primitives;
+
+public static class NunitExtensions
+{
+    public static ObjectAssertions Should<TType>(this object? target)
+    {
+        return ((TType)target).Should();
+    }
+}
