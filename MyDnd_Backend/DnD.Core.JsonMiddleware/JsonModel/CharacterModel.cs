89f57699a733af2732a50f4b876d4551e32b20e9
davide.gozzi <daddidaffi@gmail.com>
2023-04-24 14:11:51 +0200

    basic implementation

---
+++ MyDnd_Backend/DnD.Core.JsonMiddleware/JsonModel/CharacterModel.cs
@@ -0,0 +1,22 @@
+﻿namespace DnD.Core.JsonMiddleware;
+
+public class CharacterModel
+{
+    public TValue[] BaseValues { get; init; }
+    public Modificator[] Modificators { get; init; }
+    public TValue[] FixedValues { get; init; }
+}
+
+public class TValue
+{
+    public string Target { get; init; }
+    public int Value { get; init; }
+}
+
+public class Modificator
+{
+    public string Mod { get; init; }
+    public string Path { get; init; }
+
+    public int? Value { get; set; } = null;
+}
---
+++ MyDnd_Backend/DnD.Core.JsonMiddleware/ModelApplicator.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using DnD.Core.ScriptSuppliers;
+
+namespace DnD.Core.JsonMiddleware;
+
+public static class ModelApplicator
+{
+    public static Character ResolveFromModel(CharacterModel characterModel, IScriptSupplier scriptSupplier)
+    {
+        var character = new Character(scriptSupplier);
+        var characterType = typeof(Character);
+            
+        foreach (var tValue in characterModel.BaseValues)
+        {
+            var propertyInfo = characterType.GetProperty(tValue.Target);
+            propertyInfo.SetValue(character, tValue.Value);
+        }
+
+        foreach (var mod in characterModel.Modificators)
+        {
+            if (mod.Value.HasValue)
+            {
+                var propertyInfo = characterType.GetProperty(mod.Mod);
+                propertyInfo.SetValue(character, mod.Value);
+                continue;
+            }
+
+            var methodInfo = characterType.GetMethod(mod.Path);
+            methodInfo.Invoke(character, new [] { mod.Mod });
+        }
+        
+        foreach (var tValue in characterModel.FixedValues)
+        {
+            var propertyInfo = characterType.GetProperty(tValue.Target);
+            propertyInfo.SetValue(character, tValue.Value);
+        }
+        
+        return character;
+    }
+}
